d793282cf7357b80438bc9f2c393e4126c1b9da0
Bruna <brunahalves94@gmail.com>
2016-08-12 21:45:40 -0300

    Modifica Classe Aluno

--- src/java/Classes/Aluno.java
+++ src/java/Classes/Aluno.java
@@ -14,11 +14,88 @@ public class Aluno {
     private String aluno;
     private String matricula;
     private String cpf;
-    Periodo per = new Periodo();
-    Grupo gru = new Grupo();
+    private float pontos;
+    private String grupo;
     
     public Aluno(){
         
     }
     
+    public float pontuacaoTotal(Grupo grupo){
+        float pontuacaoTotal = 0;
+        
+        
+                
+        return pontuacaoTotal;
+    }
+
+    /**
+     * @return the aluno
+     */
+    public String getAluno() {
+        return aluno;
+    }
+
+    /**
+     * @param aluno the aluno to set
+     */
+    public void setAluno(String aluno) {
+        this.aluno = aluno;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the pontos
+     */
+    public float getPontos() {
+        return pontos;
+    }
+
+    /**
+     * @param pontos the pontos to set
+     */
+    public void setPontos(float pontos) {
+        this.pontos = pontos;
+    }
+
+    /**
+     * @return the grupo
+     */
+    public String getGrupo() {
+        return grupo;
+    }
+
+    /**
+     * @param grupo the grupo to set
+     */
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
 }
