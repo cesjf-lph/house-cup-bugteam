465e439823d8400548c47e1f8be9566ab3fa7e74
aluno <aluno@L4MAQ07.labcesac.net>
2016-08-12 21:18:09 -0300

    Criar classe grupo

---
+++ src/java/Classes/Grupo.java
@@ -0,0 +1,19 @@
+package Classes;
+
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Grupo {
+    ArrayList <Aluno> aluno = new ArrayList();
+    
+}
